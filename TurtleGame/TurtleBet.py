360a38c2b8dabdb8dbed5e4deadc86653d18d1d5
zotyag <gombarzolta@gmail.com>
2024-08-08 15:43:29 +0200

    Rearrange files

--- TurtleGame/TurtleBet.py
+++ TurtleGame/TurtleBet.py
@@ -1,35 +1,49 @@
-import pygame, sys
-pygame.init()
+import pygame as pg, sys, random
+pg.init()
 
-SIZE=(1000, 700)
-screen=pygame.display.set_mode(SIZE)
-clock=pygame.time.Clock()
+class Turtle(pg.sprite.Sprite):
+
+    def __init__(self,image,offset:tuple):
+        pg.sprite.Sprite.__init__(self)
+        self.image=pg.image.load(image)
+        self.rect=self.image.get_rect()
+        self.rect=self.rect.move(offset[0],offset[1])
+        self.speed=3
+        self.started=False
+        self.finished=False
+        self._counter=0
+
+    def update(self):
+        self._counter+=1
+        if self._counter==60:
+            r=random.randint(1,5)
+            self.speed=r
+            print(r)
+            self._counter=0
+        newpos=self.rect.move(self.speed,0)
+        self.rect=newpos
 
 
 
 
+SIZE=(1000,700)
+
+screen=pg.display.set_mode(SIZE)
+clock=pg.time.Clock()
+
+wt=Turtle("TurtleGame/TurtWh.png",(0,500))
 
 while True:
-    for keys in pygame.event.get():
-        if keys.type==pygame.QUIT:
-            pygame.quit()
+    for event in pg.event.get():
+        if event.type==pg.QUIT:
+            pg.quit()
             sys.exit()
-    keys=pygame.key.get_pressed()
-    if keys[pygame.K]
+    wt.update()
+    screen.fill((0,0,0))
+    screen.blit(wt.image,wt.rect)
+    pg.display.update()
 
-
-
-
-
-
-
-
-
-    pygame.display.update()
     clock.tick(60)
-
-
-
 
 
 
